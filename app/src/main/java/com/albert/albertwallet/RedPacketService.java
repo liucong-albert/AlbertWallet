5800adeab57b760358d47b179e9c94eca443fa0d
liucong-albert <515518205@qq.com>
2018-04-03 21:57:20 +0800

    init

--- app/src/main/java/com/albert/albertwallet/RedPacketService.java
+++ app/src/main/java/com/albert/albertwallet/RedPacketService.java
@@ -30,7 +30,7 @@ public class RedPacketService extends AccessibilityService {
             case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
                 openRedPack(rootNode);
                 closeRedPack(rootNode);
-                findDown(rootNode);
+//                findDown(rootNode);
                 break;
         }
     }
@@ -79,16 +79,17 @@ public class RedPacketService extends AccessibilityService {
         if (rootNode == null){
             return;
         }
-        for (int j = 0; j < rootNode.getChildCount(); j++) {
-            AccessibilityNodeInfo myinfo = rootNode.getChild(j);
-            List<AccessibilityNodeInfo> list = myinfo.findAccessibilityNodeInfosByText("红包记录");
-            if (list.size() > 0){
-                Log.e("-----------", "找到RedPacket领取页面");
-                AccessibilityNodeInfo iteminfo = list.get(0);
-                AccessibilityNodeInfo parentInfo = iteminfo.getParent();
-                doClose(parentInfo);
-                break;
+        List<AccessibilityNodeInfo> list = rootNode.findAccessibilityNodeInfosByText("红包记录");
+        if (list.size() > 0){
+            long  time1 = System.currentTimeMillis();
+            if (time1 - time3 <1000){
+                return;
             }
+            time3 = System.currentTimeMillis();
+            Log.e("-----------", "找到RedPacket领取页面");
+            AccessibilityNodeInfo iteminfo = list.get(0);
+            AccessibilityNodeInfo parentInfo = iteminfo.getParent();
+            doClose(parentInfo);
         }
     }
 
@@ -100,11 +101,6 @@ public class RedPacketService extends AccessibilityService {
                 findImg(myinfo);
             }else{
                 Log.e("-----------", "找到关闭RedPacket按钮");
-                try{
-                    Thread.sleep(200);
-                }catch ( Exception e){
-                    e.printStackTrace();
-                }
                 perforGlobalClick(myinfo);
                 return;
             }
@@ -116,16 +112,17 @@ public class RedPacketService extends AccessibilityService {
         if (rootNode == null){
             return;
         }
-        for (int j = 0; j < rootNode.getChildCount(); j++) {
-            AccessibilityNodeInfo myinfo = rootNode.getChild(j);
-            List<AccessibilityNodeInfo> list = myinfo.findAccessibilityNodeInfosByText("发了一个");
-            if (list.size() > 0){
-                Log.e("-----------", "找到RedPacket页面");
-                AccessibilityNodeInfo iteminfo = list.get(0);
-                AccessibilityNodeInfo parentInfo = iteminfo.getParent();
-                findImg(parentInfo);
-                break;
+        List<AccessibilityNodeInfo> list = rootNode.findAccessibilityNodeInfosByText("发了一个");
+        if (list.size() > 0){
+            long  time1 = System.currentTimeMillis();
+            if (time1 - time2 <1000){
+                return;
             }
+            time2 = System.currentTimeMillis();
+            Log.e("-----------", "打开RedPacket");
+            AccessibilityNodeInfo iteminfo = list.get(0);
+            AccessibilityNodeInfo parentInfo = iteminfo.getParent();
+            findImg(parentInfo);
         }
     }
 
@@ -136,12 +133,7 @@ public class RedPacketService extends AccessibilityService {
             if (!myinfo.getClassName().equals("android.widget.ImageView")) {
                 findImg(myinfo);
             }else{
-                Log.e("-----------", "找到打开RedPacket按钮");
-                try{
-                    Thread.sleep(200);
-                }catch ( Exception e){
-                    e.printStackTrace();
-                }
+                Log.e("-----------", "成功领取RedPacket");
                 perforGlobalClick(myinfo);
                 return;
             }
@@ -151,24 +143,27 @@ public class RedPacketService extends AccessibilityService {
     /**
      * 遍历
      */
+    long time = 0;
+    long time2 = 0;
+    long time3 = 0;
     synchronized private void checkData(AccessibilityNodeInfo rootNode) {
         if (rootNode == null){
             return;
         }
-        for (int j = 0; j < rootNode.getChildCount(); j++) {
-            AccessibilityNodeInfo myinfo = rootNode.getChild(j);
-            if (!myinfo.getClassName().equals("org.telegram.messenger.support.widget.RecyclerView")) {
-                checkData(myinfo);
-            } else {
-                List<AccessibilityNodeInfo> list = myinfo.findAccessibilityNodeInfosByText("领取红包");
-                Log.e("-----------", "成功领取的RedPacket "+list.size());
-                if (list.size() > 0){
-                    AccessibilityNodeInfo iteminfo = list.get(0);
-                    perforGlobalClick(iteminfo);
-                    Log.e("-----------", "领取完成点击返回");
-                    return;
-                }
+        List<AccessibilityNodeInfo> list = rootNode.findAccessibilityNodeInfosByText("领取红包");
+        if (list.size() > 0){
+            Log.e("-----------", "time = "+time);
+            long  time1 = System.currentTimeMillis();
+            if (time1 - time <1000){
+                return;
             }
+            time = System.currentTimeMillis();
+            Log.e("-----------", "time  init = "+time);
+
+            AccessibilityNodeInfo iteminfo = list.get(0);
+            perforGlobalClick(iteminfo);
+            Log.e("-----------", "发现RedPacket");
+            return;
         }
     }
 
@@ -198,243 +193,8 @@ public class RedPacketService extends AccessibilityService {
             dataOutputStream.flush();
             dataOutputStream.close();
             outputStream.close();
-//            process.waitFor();
         } catch (Throwable t) {
             t.printStackTrace();
         }
     }
 }
-
-//                if (text != null){
-//                    AccessibilityNodeInfo parent = node1.getParent();
-//                    while (parent != null) {
-//                        Log.e("-----------",""+text);
-//                        if (parent.isClickable()) {
-//                            //模拟点击
-////                        parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//                            //isOpenRP用于判断该红包是否点击过
-//                            Log.e("-----------","找到红包");
-//                            break;
-//                        }
-//                        parent = parent.getParent();
-//                    }
-//
-//                }else{
-//                    Log.e("-----------","text 为空");
-//                }
-//            }
-//
-//        }
-//        if (rootNode != null) {
-//            //获取所有聊天的线性布局
-//            List<AccessibilityNodeInfo> listChatRecord = rootNode.findAccessibilityNodeInfosByViewId("com.telegram.btcchat:id/o");
-//            if(listChatRecord.size()==0){
-//                return;
-//            }
-            //获取最后一行聊天的线性布局（即是最新的那条消息）
-//            AccessibilityNodeInfo finalNode = listChatRecord.get(listChatRecord.size() - 1);
-            //获取聊天对象list（其实只有size为1）
-//            List<AccessibilityNodeInfo> imageName = finalNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/i_");
-            //获取聊天信息list（其实只有size为1）
-//            List<AccessibilityNodeInfo> record = finalNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ib");
-//        }
-//    }
-//
-//    /**
-//     * 必须重写的方法：系统要中断此service返回的响应时会调用。在整个生命周期会被调用多次。
-//     */
-//    @Override
-//    public void onInterrupt() {
-//        Toast.makeText(this, "我快被终结了啊-----", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    /**
-//     * 服务开始连接
-//     */
-//    @Override
-//    protected void onServiceConnected() {
-//        Toast.makeText(this, "服务已开启", Toast.LENGTH_SHORT).show();
-//        super.onServiceConnected();
-//    }
-//
-//    /**
-//     * 服务断开
-//     *
-//     * @param
-//     * @return
-//     */
-//    @Override
-//    public boolean onUnbind(Intent intent) {
-//        Toast.makeText(this, "服务已被关闭", Toast.LENGTH_SHORT).show();
-//        return super.onUnbind(intent);
-//    }
-//}
-//        switch (eventType) {
-//            //界面跳转的监听
-//            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-//                String className = event.getClassName().toString();
-//                //判断是否是微信聊天界面
-//                if (LAUCHER.equals(className)) {
-//                    //获取当前聊天页面的根布局
-//                    AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-//                    //开始找红包
-//                    findRedPacket(rootNode);
-//                }
-//
-//                //判断是否是显示‘开’的那个红包界面
-//                if (LUCKEY_MONEY_RECEIVER.equals(className)) {
-//                    AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-//                    //开始抢红包
-//                    openRedPacket(rootNode);
-//                }
-//
-//                //判断是否是红包领取后的详情界面
-//                if (isOpenDetail && LUCKEY_MONEY_DETAIL.equals(className)) {
-//
-//                    isOpenDetail = false;
-//                    //返回桌面
-//                    back2Home();
-//                    //如果之前是锁着屏幕的则重新锁回去
-//                    release();
-//                }
-//                break;
-//        }
-//    }
-//
-//    /**
-//     * 开始打开红包
-//     */
-//    private void openRedPacket(AccessibilityNodeInfo rootNode) {
-//        for (int i = 0; i < rootNode.getChildCount(); i++) {
-//            AccessibilityNodeInfo node = rootNode.getChild(i);
-//            if ("android.widget.ImageView".equals(node.getClassName())) {
-//                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//
-//                isOpenDetail = true;
-//            }
-//            openRedPacket(node);
-//        }
-//    }
-//
-//    /**
-//     * 遍历查找红包
-//     */
-//    private void findRedPacket(AccessibilityNodeInfo rootNode) {
-//        if (rootNode != null) {
-//            //从最后一行开始找起
-//            for (int i = rootNode.getChildCount() - 1; i >= 0; i--) {
-//                AccessibilityNodeInfo node = rootNode.getChild(i);
-//                //如果node为空则跳过该节点
-//                if (node == null) {
-//                    continue;
-//                }
-//                CharSequence text = node.getText();
-//                if (text != null && text.toString().equals("领取红包")) {
-//                    AccessibilityNodeInfo parent = node.getParent();
-//                    //while循环,遍历"领取红包"的各个父布局，直至找到可点击的为止
-//                    while (parent != null) {
-//                        if (parent.isClickable()) {
-//                            //模拟点击
-//                            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-//                            //isOpenRP用于判断该红包是否点击过
-//                            isOpenRP = true;
-//
-//                            break;
-//                        }
-//                        parent = parent.getParent();
-//                    }
-//                }
-//                //判断是否已经打开过那个最新的红包了，是的话就跳出for循环，不是的话继续遍历
-//                if (isOpenRP) {
-//                    break;
-//                } else {
-//                    findRedPacket(node);
-//                }
-//
-//            }
-//        }
-//    }
-//
-//    /**
-//     * 服务连接
-//     */
-//    @Override
-//    protected void onServiceConnected() {
-//        Toast.makeText(this, "抢红包服务开启", Toast.LENGTH_SHORT).show();
-//        super.onServiceConnected();
-//    }
-//
-//    /**
-//     * 必须重写的方法：系统要中断此service返回的响应时会调用。在整个生命周期会被调用多次。
-//     */
-//    @Override
-//    public void onInterrupt() {
-//        Toast.makeText(this, "我快被终结了啊-----", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    /**
-//     * 服务断开
-//     */
-//    @Override
-//    public boolean onUnbind(Intent intent) {
-//        Toast.makeText(this, "抢红包服务已被关闭", Toast.LENGTH_SHORT).show();
-//        return super.onUnbind(intent);
-//    }
-//
-//    /**
-//     * 返回桌面
-//     */
-//    private void back2Home() {
-//        Intent home = new Intent(Intent.ACTION_MAIN);
-//        home.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//        home.addCategory(Intent.CATEGORY_HOME);
-//        startActivity(home);
-//    }
-//
-//    /**
-//     * 判断是否处于亮屏状态
-//     *
-//     * @return true-亮屏，false-暗屏
-//     */
-//    private boolean isScreenOn() {
-//        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-//        isScreenOn = pm.isScreenOn();
-//        Log.e("isScreenOn", isScreenOn + "");
-//        return isScreenOn;
-//    }
-//
-//    /**
-//     * 解锁屏幕
-//     */
-//    private void wakeUpScreen() {
-//
-//        //获取电源管理器对象
-//        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-//        //后面的参数|表示同时传入两个值，最后的是调试用的Tag
-//        wakeLock = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.FULL_WAKE_LOCK, "bright");
-//
-//        //点亮屏幕
-//        wakeLock.acquire();
-//
-//        //得到键盘锁管理器
-//        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
-//        keyguardLock = km.newKeyguardLock("unlock");
-//
-//        //解锁
-//        keyguardLock.disableKeyguard();
-//    }
-//
-//    /**
-//     * 释放keyguardLock和wakeLock
-//     */
-//    public void release() {
-//        if (keyguardLock != null) {
-//            keyguardLock.reenableKeyguard();
-//            keyguardLock = null;
-//        }
-//        if (wakeLock != null) {
-//            wakeLock.release();
-//            wakeLock = null;
-//        }
-//    }
-
